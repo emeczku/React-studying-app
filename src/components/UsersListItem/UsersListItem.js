a8d3486a1a0eb231304fa6a77ac45aec069a0b18
Patryk Majewski <dzius89@gmail.com>
2022-06-20 15:15:44 +0200

    installed styled components, created wrappers for users list

--- src/components/UsersListItem/UsersListItem.js
+++ src/components/UsersListItem/UsersListItem.js
@@ -1,22 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Wrapper = styled.li`
+  display: flex;
+  align-items: center;
+  position: relative;
+  &:not(:last-child)::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 1px;
+    background-color: lightgrey;
+  }
+`;
+
+const StyledButton = styled.button`
+  width: 40px;
+  height: 40px;
+  background-color: #c0c7d6;
+  border-radius: 50px;
+  border: none;
+  color: white;
+`;
 
 const UsersListItem = ({ userData: { average, name, attendance } }) => (
-  <li>
+  <Wrapper>
     <div>{average}</div>
     <div>
       <p>{name}</p>
       <p>{attendance}</p>
     </div>
-    <button>X</button>
-  </li>
+    <StyledButton>X</StyledButton>
+  </Wrapper>
 );
 
 UsersListItem.propTypes = {
   userData: PropTypes.shape({
     average: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
-    attendance: PropTypes.string.isRequired,
+    attendance: PropTypes.string,
   }),
 };
 
